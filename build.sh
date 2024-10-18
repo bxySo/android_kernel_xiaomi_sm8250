9ad8500da150c83628555863da9ad091765f93e2
bxySo <bxyso@outlook.com>
2024-10-18 18:47:49 +0800

    build.sh: Opt

--- build.sh
+++ build.sh
@@ -16,127 +16,118 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if ! [ -d "$HOME/tc/aosp-clang" ]; then
-echo "aosp clang not found! Cloning..."
-if ! git clone -q https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone.git --depth=1 ~/tc/aosp-clang; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
+CLANG_DIR=~/android/aospa/prebuilts/clang/host/linux-x86/clang-r498229b
+GCC_64_DIR=~/android/aospa/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 
-if ! [ -d "$HOME/tc/aarch64-linux-android-4.9" ]; then
-echo "aarch64-linux-android-4.9 not found! Cloning..."
-if ! git clone -q https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 --single-branch ~/tc/aarch64-linux-android-4.9; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
-
-GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
-KBUILD_COMPILER_STRING=$($HOME/tc/aosp-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-KBUILD_LINKER_STRING=$($HOME/tc/aosp-clang/bin/ld.lld --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' | sed 's/(compatible with [^)]*)//')
-export KBUILD_COMPILER_STRING
-export KBUILD_LINKER_STRING
-
-DEVICE=$1
-
-if [ "${DEVICE}" = "alioth" ]; then
-DEFCONFIG=vendor/alioth_defconfig
-elif [ "${DEVICE}" = "apollo" ]; then
-DEFCONFIG=vendor/apollo_defconfig
-elif [ "${DEVICE}" = "lmi" ]; then
-DEFCONFIG=vendor/lmi_defconfig
-elif [ "${DEVICE}" = "munch" ]; then
-DEFCONFIG=vendor/munch_defconfig
-elif [ "${DEVICE}" = "psyche" ]; then
-DEFCONFIG=vendor/psyche_defconfig
-fi
-
-#
-# Enviromental Variables
-#
+DEVICE=${1:?"No device parameter provided. Usage: $0 <device>"}
+DEFCONFIG=${DEVICE}_defconfig
 
 DATE=$(date '+%Y%m%d-%H%M')
+LOCALVERSION=$(sed -n 's/^.*CONFIG_LOCALVERSION="\([^"]*\)".*$/\1/p' arch/arm64/configs/"$DEFCONFIG")
+zipname="AnyKernel3-${DEVICE}${LOCALVERSION}-${DATE}.zip"
 
-# Set our directory
-OUT_DIR=out/
+function Build() {
+	START=$(date +"%s")
 
-VERSION="Uvite-${DEVICE}-${DATE}"
-
-# Export Zip name
-export ZIPNAME="${VERSION}.zip"
-
-# How much kebabs we need? Kanged from @raphielscape :)
-if [[ -z "${KEBABS}" ]]; then
-    COUNT="$(grep -c '^processor' /proc/cpuinfo)"
-    export KEBABS="$((COUNT + 2))"
-fi
-
-echo "Jobs: ${KEBABS}"
-
-ARGS="ARCH=arm64 \
-O=${OUT_DIR} \
-CC=clang \
-LLVM=1 \
-LLVM_IAS=1 \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
--j${KEBABS}"
-
-dts_source=arch/arm64/boot/dts/vendor/qcom
-
-START=$(date +"%s")
-
-# Set compiler Path
-export PATH="$HOME/tc/aosp-clang/bin:$PATH"
-export LD_LIBRARY_PATH=${HOME}/tc/aosp-clang/lib64:$LD_LIBRARY_PATH
-
-echo "------ Starting Compilation ------"
-
-# Make defconfig
-make -j${KEBABS} ${ARGS} ${DEFCONFIG}
-
-# Make olddefconfig
-cd ${OUT_DIR} || exit
-make -j${KEBABS} ${ARGS} CC="ccache clang" HOSTCC="ccache gcc" HOSTCXX="cache g++" olddefconfig
-cd ../ || exit
-
-make -j${KEBABS} ${ARGS} CC="ccache clang" HOSTCC="ccache gcc" HOSTCXX="ccache g++" 2>&1 | tee build.log
-
-find ${OUT_DIR}/$dts_source -name '*.dtb' -exec cat {} + >${OUT_DIR}/arch/arm64/boot/dtb
-
-git checkout arch/arm64/boot/dts/vendor &>/dev/null
-
-echo "------ Finishing Build ------"
-
-END=$(date +"%s")
-DIFF=$((END - START))
-zipname="$VERSION.zip"
-if [ -f "out/arch/arm64/boot/Image" ] && [ -f "out/arch/arm64/boot/dtbo.img" ] && [ -f "out/arch/arm64/boot/dtb" ]; then
-        if [ "${DEVICE}" = "alioth" ]; then
-          git clone -q https://github.com/madmax7896/AnyKernel3.git -b alioth
-        elif [ "${DEVICE}" = "apollo" ]; then
-          git clone -q https://github.com/madmax7896/AnyKernel3.git -b apollo
-        elif [ "${DEVICE}" = "lmi" ]; then
-          git clone -q https://github.com/madmax7896/AnyKernel3.git -b lmi
-        elif [ "${DEVICE}" = "munch" ]; then
-          git clone -q https://github.com/madmax7896/AnyKernel3.git -b munch-uvite
-        else
-          git clone -q https://github.com/madmax7896/AnyKernel3.git -b psyche
+	if ! [ -d "${CLANG_DIR}" ]; then
+		echo "aosp clang not found! Cloning..."
+		if ! git clone -q https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone.git --depth=1 $CLANG_DIR; then
+			echo "Cloning failed! Aborting..."
+			exit 1
+		fi
 	fi
-	cp out/arch/arm64/boot/Image AnyKernel3
-	cp out/arch/arm64/boot/dtb AnyKernel3
-	cp out/arch/arm64/boot/dtbo.img AnyKernel3
-	rm -f *zip
-	cd AnyKernel3
-	zip -r9 "../${zipname}" * -x '*.git*' README.md *placeholder >> /dev/null
+
+	if ! [ -d "${GCC_64_DIR}" ]; then
+		echo "aarch64-linux-android-4.9 not found! Cloning..."
+		if ! git clone -q https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 --single-branch $GCC_64_DIR; then
+			echo "Cloning failed! Aborting..."
+			exit 1
+		fi
+	fi
+
+	KBUILD_COMPILER_STRING=$(${CLANG_DIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	KBUILD_LINKER_STRING=$(${CLANG_DIR}/bin/ld.lld --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' | sed 's/(compatible with [^)]*)//')
+	export KBUILD_COMPILER_STRING
+	export KBUILD_LINKER_STRING
+
+	#
+	# Enviromental Variables
+	#
+
+	# Set our directory
+	OUT_DIR=out/
+
+	# How much kebabs we need? Kanged from @raphielscape :)
+	if [[ -z "${KEBABS}" ]]; then
+		COUNT="$(grep -c '^processor' /proc/cpuinfo)"
+		export KEBABS="$((COUNT + 2))"
+	fi
+
+	echo "Jobs: ${KEBABS}"
+
+	ARGS="ARCH=arm64 \
+		O=${OUT_DIR} \
+		CC=clang \
+		LLVM=1 \
+		LLVM_IAS=1 \
+		CLANG_TRIPLE=aarch64-linux-gnu- \
+		CROSS_COMPILE=${GCC_64_DIR}/bin/aarch64-linux-android- \
+		-j${KEBABS}"
+
+	dts_source=arch/arm64/boot/dts/vendor/qcom
+
+	# Set compiler Path
+	export PATH="${CLANG_DIR}/bin:$PATH"
+	export LD_LIBRARY_PATH=${CLANG_DIR}/lib64:$LD_LIBRARY_PATH
+
+	echo "------ Starting Compilation ------"
+
+	# Make defconfig
+	make -j${KEBABS} ${ARGS} ${DEFCONFIG}
+
+	# Make olddefconfig
+	cd ${OUT_DIR} || exit
+	make -j${KEBABS} ${ARGS} CC="ccache clang" HOSTCC="ccache gcc" HOSTCXX="cache g++" olddefconfig
+	cd ../ || exit
+
+	make -j${KEBABS} ${ARGS} CC="ccache clang" HOSTCC="ccache gcc" HOSTCXX="ccache g++" 2>&1 | tee build.log
+
+	find ${OUT_DIR}/$dts_source -name '*.dtb' -exec cat {} + >${OUT_DIR}/arch/arm64/boot/dtb
+
+	git checkout arch/arm64/boot/dts/vendor &>/dev/null
+
+	echo "------ Finishing Build ------"
+
+	END=$(date +"%s")
+	DIFF=$((END - START))
+	echo "Took $DIFF second(s) !"
+}
+
+function AnyKernel3() {
+	echo -e "\nPackaging into AnyKernel3..."
+	if [ ! -f "out/arch/arm64/boot/Image" ] \
+	|| [ ! -f "out/arch/arm64/boot/dtbo.img" ] \
+	|| [ ! -f "out/arch/arm64/boot/dtb" ]; then
+		echo -e "\n Compilation Failed!"
+		exit 1
+	fi
+
+	if [ ! -d ./AnyKernel3 ]; then
+		git clone https://github.com/madmax7896/AnyKernel3 -b "$DEVICE" --depth=1 || return 1
+	fi
+
+	cp -r AnyKernel3 ak3-tmp
+	cp out/arch/arm64/boot/Image ak3-tmp
+	cp out/arch/arm64/boot/dtb ak3-tmp
+	cp out/arch/arm64/boot/dtbo.img ak3-tmp
+	rm -f ./*zip
+	cd ak3-tmp || exit
+	zip -r9 "../${zipname}" ./* -x '*.git*' README.md ./*placeholder >>/dev/null
 	cd ..
-	rm -rf AnyKernel3
+	rm -rf ak3-tmp
 	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-	echo ""
-	echo -e ${zipname} " is ready!"
-	echo ""
-        curl --upload-file ${zipname} https://free.keep.sh
-else
-	echo -e "\n Compilation Failed!"
-fi
+	echo -e "\n${zipname} is ready!"
+}
+
+Build "$@"
+AnyKernel3
